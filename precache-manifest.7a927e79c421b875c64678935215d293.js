b3e385faca9575476dbb1943f63d1d58fad128ef
Ashay Thorat <in-ashayt@Ashays-MacBook-Pro-3.local>
2019-12-03 00:12:18 +0530

    1

---
+++ precache-manifest.7a927e79c421b875c64678935215d293.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c8012c7fe06bbbf58007b150cd99df7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bda10212736a0f589789",
+    "url": "/static/css/2.83ace7dd.chunk.css"
+  },
+  {
+    "revision": "cd5a09699144ffc4cac5",
+    "url": "/static/css/main.b1012d74.chunk.css"
+  },
+  {
+    "revision": "bda10212736a0f589789",
+    "url": "/static/js/2.a438bc58.chunk.js"
+  },
+  {
+    "revision": "cd5a09699144ffc4cac5",
+    "url": "/static/js/main.de0f9140.chunk.js"
+  },
+  {
+    "revision": "1e01a87baaa967cac84c",
+    "url": "/static/js/runtime-main.40884d3c.js"
+  }
+]);
